b562923cbebe2acf0babb5b725c350a80efd23eb
ChilenoSika <jaum97@yahoo.com.br>
2018-11-03 02:36:42 -0300

    Still with the basics

--- bundle.js
+++ bundle.js
@@ -1,19 +1,91 @@
 "use strict";
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 var array = [1, 2, 3, 6, 7, 8, 0];
 var newArray = array.map(function (item, index) {
   return item * index;
 });
-console.log(newArray);
 var sumArray = array.reduce(function (total, next) {
   return total + next;
 });
-console.log(sumArray);
 var filterArray = array.filter(function (item) {
   return item % 2 === 0;
 });
-console.log(filterArray);
 var findArray = array.find(function (item) {
   return item === 2;
 });
-console.log(findArray);
+var arr = [2, 3, 5, 6, 7, 1, 0];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+var user = [{
+  name: 'Joao',
+  age: 21,
+  address: {
+    city: 'campinas',
+    street: 'sesame street',
+    number: 355,
+    ap: 201
+  }
+}, {
+  name: 'Rodrigo',
+  age: 33,
+  address: {
+    city: 'new york',
+    street: 'street unknow',
+    number: 344,
+    ap: 202
+  }
+}]; //const { name, ...leftover } = user[0];
+
+var showInfo = function showInfo() {
+  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : user[0];
+
+  var name = _ref.name,
+      leftover = _objectWithoutProperties(_ref, ["name"]);
+
+  return console.log(leftover);
+};
+
+showInfo();
+
+var sum = function sum() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+};
+
+var arrr1 = [1, 2, 3];
+var arrr2 = [4, 5, 6];
+var arrr3 = arrr1.concat(arrr2);
+
+var user2 = _objectSpread({}, user[0], {
+  name: 'Potato'
+});
+
+var templateLiteral = function templateLiteral(user) {
+  var nome = user.name,
+      idade = user.age;
+  console.log("O ".concat(nome, " tem ").concat(idade, " anos"));
+};
+
+templateLiteral(user[0]);
+var language = 'Japanese';
+var country = 'Japan';
+var newOBJ = {
+  language: language,
+  country: country,
+  city: 'tokyo'
+};
+console.log(newOBJ);
--- main.js
+++ main.js
@@ -3,23 +3,73 @@ const array = [1, 2, 3, 6, 7, 8, 0];
 const newArray = array.map(function (item, index) {
     return item * index;
 });
-
-console.log(newArray);
-
 const sumArray = array.reduce(function (total, next) {
     return total + next;
 });
-
-console.log(sumArray);
-
 const filterArray = array.filter(function (item) {
     return item % 2 === 0;
 });
-
-console.log(filterArray);
-
 const findArray = array.find(function (item) {
     return item === 2;
 });
 
-console.log(findArray)
+const arr = [2, 3, 5, 6, 7, 1, 0];
+
+const newArr = arr.map(item => item * 2);
+
+const user = [{
+    name: 'Joao',
+    age: 21,
+    address: {
+        city: 'campinas',
+        street: 'sesame street',
+        number: 355,
+        ap: 201
+    }
+
+},
+{
+    name: 'Rodrigo',
+    age: 33,
+    address: {
+        city: 'new york',
+        street: 'street unknow',
+        number: 344,
+        ap: 202
+    }
+
+}
+]
+//const { name, ...leftover } = user[0];
+
+const showInfo = ({ name, ...leftover } = user[0]) => console.log(leftover);
+
+
+showInfo();
+
+const sum = (...params) => params.reduce((total, next) => total + next);
+
+const arrr1 = [1, 2, 3];
+const arrr2 = [4, 5, 6];
+
+const arrr3 = [...arrr1, ...arrr2];
+
+const user2 = { ...user[0], name: 'Potato' };
+
+const templateLiteral = (user) => {
+    const { name: nome, age: idade } = user;
+    console.log(`O ${nome} tem ${idade} anos`);
+}
+
+templateLiteral(user[0]);
+
+const language = 'Japanese';
+const country = 'Japan';
+
+const newOBJ = {
+    language,
+    country,
+    city: 'tokyo'
+}
+
+console.log(newOBJ);
